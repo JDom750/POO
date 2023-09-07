61efe216a68bb1c94a90bff096d1872dae065ed5
JDom750 <Joaquindominguez750@gmail.com>
2023-09-06 22:31:59 -0300

    Ejercicios resueltos

--- src/com/Parte_Juego/Main.java
+++ src/com/Parte_Juego/Main.java
@@ -1,14 +1,17 @@
 package com.Parte_Juego;
 
 public class Main {
-    //Cramos los Jugadores
-    Jugador J1 = new Jugador("Luciano");
-    Jugador J2 = new Jugador("Agustin");
+    public static void main(String[] args) {
+        // Creamos los Jugadores
+        Jugador J1 = new Jugador("Luciano");
+        Jugador J2 = new Jugador("Agustin");
 
-    //Le asignamos una palabra
-                    J1.Agregar_Palabra_Valida("Viaje");
-                    J2.Agregar_Palabra_Valida("Kiwi");
+        // Le asignamos una palabra
+        J1.Agregar_Palabra_Valida("Viaje");
+        J2.Agregar_Palabra_Valida("Kiwi");
 
-    //Punteje final y Resultado del ganardo o empate.
-                    J1.PuntajesValorTotal(J1,J2);
+        // Punteje final y resultado del ganador o empate.
+        J1.PuntajesValorTotal(J1, J2);
+    }
 }
+
